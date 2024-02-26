496872eb0d68d4b92fd4a6324bf9f2e46cd0339a
aadityapattabhiraman <154038229+aadityapattabhiraman@users.noreply.github.com>
2024-02-26 19:30:04 +0530

    LayerNormalization

---
+++ model.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import torch
+import torch.nn as nn
+import math
+
+
+class LayerNormalization(nn.Module):
+
+    def __init__(self, features: int, eps: float=10**-6) -> None:
+        super().__init__()
+        self.eps = eps
+        self.alpha = nn.Parameter(torch.ones(features))
+        self.bias = nn.Parameter(torch.zeros(features))
+
+    def forward(self, x):
+        mean = x.mean(dim=-1, keepdim=True)
+        std = x.std(dim=-1, keepdim=True)
+        return self.alpha * (x - mean) / (std + self.eps) + self.bias
