64ec39c3266198bd98f3d728d38b51e9128b36f9
aadityapattabhiraman <154038229+aadityapattabhiraman@users.noreply.github.com>
2024-02-26 19:51:30 +0530

    PositionalEncoding comments

--- model.py
+++ model.py
@@ -43,14 +43,21 @@ class InputEmbeddings(nn.Module):
         self.d_model = d_model
         self.seq_len = seq_len
         self.dropout = nn.Dropout(dropout)
+        # Create a matrix of shape (seq_len, d_model)
         pe = torch.zeros(seq_len, d_model)
+        # Create a vector of shape (seq_len)
         position = torch.arange(0, seq_len, dtype=torch.float).unsqueeze(1)
+        # Create a vector of shape (d_model)
         div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
+        # Apply sine to even indices
         pe[:, 0::2] = torch.sin(position * div_term)
+        # Apply cosine to odd indices
         pe[:, 1::2] = torch.cos(position * div_term)
-        pe = pe.unsqueeze(0)
+        # Add a batch dimension to the positional encoding
+        pe = pe.unsqueeze(0) # (1, seq_len, d_model)
+        # Register the positional encoding as a buffer
         self.register_buffer("pe", pe)
 
     def forward(self, x):
-        x = x + (self.pe[:, :x.shape[1], :]).requires_grad_(False)
+        x = x + (self.pe[:, :x.shape[1], :]).requires_grad_(False) # (batch, seq_len, d_mdoel)
         return self.dropout(x)
