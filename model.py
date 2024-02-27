fc0e1404dfbc8d1dd4c9ff118afdb64ea2706eba
aadityapattabhiraman <154038229+aadityapattabhiraman@users.noreply.github.com>
2024-02-27 14:09:19 +0530

    Decoder

--- model.py
+++ model.py
@@ -168,3 +168,16 @@ class DecoderBlock(nn.Module):
         x = self.residual_connections[1](x, lambda x: self.cross_attention_block(x, encoder_output, encoder_output, src_mask))
         x = self.residual_connections[2](x, self.feed_forward_block)
         return x
+
+
+class Decoder(nn.Module):
+
+    def __init__(self, features: int, layers: nn.ModuleList) -> None:
+        super().__init__()
+        self.layers = layers
+        self.norm = LayerNormalization(features)
+
+    def forward(self, x, encoder_output, src_mask, tgt_mask):
+        for layer in self.layers:
+            x = layer(x, encoder_output, src_mask, tgt_mask)
+        return self.norm(x)
