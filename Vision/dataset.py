bb750d408ddc1eac29ba5694928ff6b4db4fc120
aadityapattabhiraman <154038229+aadityapattabhiraman@users.noreply.github.com>
2024-03-04 15:23:44 +0530

    go

--- Vision/dataset.py
+++ Vision/dataset.py
@@ -7,13 +7,7 @@ from torch import nn
 # define the device
 device = "cuda" if torch.cuda.is_available() else "mps" if torch.has_mps or torch.backends.mps.is_available() else "cpu"
 print("Using device:", device)
-if device == "cuda":
-    print(f"Device name: {torch.cuda.get_device_name(device.index)}")
-    print(f"Device memory: {torch.cuda.get_device_properties(device.index).total_memory / 1024 ** 3} GB")
-elif (device == "mps"):
-    print(f"Device name: <mps>")
-else:
-    print("NOTE: If you have a GPU, consider using it or training.")
-device = torch.device(device)
 
-# Make sure the weights
+image_path = "/home/akugyo/GitHub/final-year/Vision/pizza_steak_sushi/"
+train_dir = image_path / "train"
+test_dir = image_path / "test"
