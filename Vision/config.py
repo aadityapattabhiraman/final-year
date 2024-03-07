f7e2dfe620fcd404d23399758dd259e50bf6d39e
aadityapattabhiraman <154038229+aadityapattabhiraman@users.noreply.github.com>
2024-03-07 19:55:42 +0530

    go

---
+++ Vision/config.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+config = {
+    "batch_size": 32,
+    "image_size": 224,
+    "color_channels": 3,
+    "patch_size": 16,
+}
--- Vision/vit.py
+++ Vision/vit.py
@@ -1,1 +1,7 @@
 #!/usr/bin/env python3
+
+import data_setup
+from torchvision import transforms
+import matplotlib.pyplot as plt
+from dataset import dataset
+from helper_functions import download_data
