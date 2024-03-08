6791c483e4edaaf9ad382d7a9f9ce2a942b8d047
aadityapattabhiraman <154038229+aadityapattabhiraman@users.noreply.github.com>
2024-03-08 11:23:26 +0530

    vision

--- Vision/model.py
+++ Vision/model.py
@@ -27,3 +27,52 @@ class PatchEmbed(nn.Module):
 
 
 class Attention(nn.Module):
+    def __init__(self, dim, n_heads=12, qkv_bias=True, attn_p=0., proj_p=0.):
+        super.__init__()
+        self.n_heads = n_heads
+        self.dim = dim
+        self.head_dim = dim // n_heads
+        self.scale = self.head_dim ** -0.5
+        self.qkv = nn.Linear(dim, dim * 3, bias=qkv_bias)
+        self.attn_drop = nn.Dropout(attn_p)
+        self.proj = nn.Linear(dim, dim)
+        self.proj_drop = nn.Dropout(proj_p)
+
+    def forward(self, x):
+        n_samples, n_tokens, dim = x.shape
+
+        if dim != self.dim:
+            raise ValueError
+
+        qkv = self.qkv(x)
+        qkv = qkv.reshape(
+            n_samples,
+            n_tokens,
+            3,
+            self.n_heads,
+            self.head_dim,
+        )
+
+        qkv = qkv.permute(
+            2, 0, 3, 1, 4
+        )
+
+        q, k, v = qkv[0], qkv[1], qkv[2]
+        k_t = k.transpose(-2, -1)
+        dp = (
+            q @ k_t
+        ) * self.scale
+        attn = dp.softmax(dim=-1)
+        attn = self.attn_drop(attn)
+
+        weighted_avg = attn @ v
+        weighted_avg = weighted_avg.transpose(1, 2)
+        weighted_avg = weighted_avg.flatten(2)
+
+        x = self.proj(weighted_avg)
+        x = self.proj_drop(x)
+
+        return x
+
+
+class MLP(nn.Module):
