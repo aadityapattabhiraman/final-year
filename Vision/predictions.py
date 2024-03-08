5f20f8f06f939cbdb31d56a3b20d5eb6745b33df
aadityapattabhiraman <154038229+aadityapattabhiraman@users.noreply.github.com>
2024-03-08 19:23:17 +0530

    vit

---
+++ Vision/predictions.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+from typing import List, Tuple
+import matplotlib.pyplot as plt
+from torchvision import transforms
+from PIL import Image
+
+
+def pred(model: torch.nn.Module, weights, image_path: str, image_size: Tuple[int, int]=(224,224),
+         transform: torchvision.transforms=None, device: torch.device="cuda"):
+    img = Image.open(image_path)
+
+    if transform is not None:
+        image_transform = transform
+    else:
+        image_transform = transforms.Compose([
+            transforms.Resize(image_size),
+            transforms.ToTensor(),
+            transforms.Normalize(
+                mean=[0.485, 0.456, 0.406],
+                std=[0.229, 0.224, 0.225]),
+        ])
+
+    model.to(device)
+    model.eval()
+
+    with torch.inference_mode():
+        transformed_image = image_transform(img).unsqueeze(dim=0)
+        target_image_pred = model(transformed_image.to(device))
+
+    target_image_pred_probs = torch.softmax(target_image_pred, dim=1)
+    class_id = target_image_pred_probs.argmax().item()
+    score = prediction[class_id].item()
+    category_name = weights.meta["categories"][class_id]
+
+    plt.figure()
+    plt.imshow(img)
+    plt.title(
+        f"Pred: {category_name} | Prob: {100 * score:.1f}%"
+    )
+    plt.axis(False)
+    plt.show()
--- Vision/vit.py
+++ Vision/vit.py
@@ -2,45 +2,13 @@
 
 import torchvision
 from model import vision_transformer
-from typing import List, Tuple
-import matplotlib.pyplot as plt
-from torchvision import transforms
-from PIL import Image
+from predictions import pred
+from torchvision.models._meta import _IMAGENET_CATEGORIES
 
 
 vit_weights = torchvision.models.ViT_L_16_Weights.DEFAULT
-model = vision_transformer(patch_size=16, mlp_dim=4096, hidden_dim=1024, num_heads=16, num_layers=24, weights=vit_weights, progress=True)
-print(model)
+model = vision_transformer(patch_size=16, mlp_dim=4096, hidden_dim=1024, num_heads=16, num_layers=24,
+                           weights=vit_weights, progress=True)
+img_path = str(input("Enter image path: "))
 
-
-def pred(model: torch.nn.Module, class_names: List[str], image_path: str, image_size: Tuple[int, int]=(224,224),
-         transform: torchvision.transforms=None, device: torch.device="cuda"):
-    img = Image.open(image_path)
-
-    if transform is not None:
-        image_transform = transform
-    else:
-        image_transform = transforms.Compose([
-            transforms.Resize(image_size),
-            transforms.ToTensor(),
-            transforms.Normalize(
-                mean=[0.485, 0.456, 0.406],
-                std=[0.229, 0.224, 0.225]),
-        ])
-
-    model.to(device)
-    model.eval()
-
-    with torch.inference_mode():
-        transformed_image = image_transform(img).unsqueeze(dim=0)
-        target_image_pred = model(transformed_image.to(device))
-
-    target_image_pred_probs = torch.softmax(target_image_pred, dim=1)
-
-    plt.figure()
-    plt.imshow(img)
-    plt.title(
-        f"Pred: {class_names[target_image_pred_label]} | Prob: {target_image_pred_probs.max():.3f}"
-    )
-    plt.axis(False)
-    plt.show()
+pred(model=model, weights=vit_weights, image_path=img_path)
